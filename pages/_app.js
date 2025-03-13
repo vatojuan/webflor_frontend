4c89f070acb4cea7d5fd4b12c54737cb064e5e4a
vatojuan <juanchi190_16@hotmail.com>
2025-03-13 02:43:50 -0300

    primeras metricas en dashboard12345final23

--- pages/_app.js
+++ pages/_app.js
@@ -1,27 +1,23 @@
-// pages/_app.js
 import { useState, useMemo, useEffect } from "react";
 import { ThemeProvider, createTheme } from "@mui/material/styles";
 import CssBaseline from "@mui/material/CssBaseline";
-import useMediaQuery from "@mui/material/useMediaQuery";
 import Head from "next/head";
 
-export default function App({ Component, pageProps }) {
-  const prefersDarkMode = useMediaQuery("(prefers-color-scheme: dark)");
+function MyApp({ Component, pageProps }) {
   const [mode, setMode] = useState("light");
 
+  // Usamos una key diferente para el admin, por ejemplo "adminColorMode"
   useEffect(() => {
-    const storedMode = localStorage.getItem("colorMode");
+    const storedMode = localStorage.getItem("adminColorMode");
     if (storedMode) {
       setMode(storedMode);
-    } else {
-      setMode(prefersDarkMode ? "dark" : "light");
     }
-  }, [prefersDarkMode]);
+  }, []);
 
   const toggleDarkMode = () => {
     setMode((prevMode) => {
       const newMode = prevMode === "light" ? "dark" : "light";
-      localStorage.setItem("colorMode", newMode);
+      localStorage.setItem("adminColorMode", newMode);
       return newMode;
     });
   };
@@ -38,9 +34,6 @@ export default function App({ Component, pageProps }) {
           secondary: {
             main: "#103B40",
           },
-          accent: {
-            main: "#2F4F4F",
-          },
           background: {
             default: mode === "light" ? "#F2E6CE" : "#2B1B17",
             paper: mode === "light" ? "#FFFFFF" : "#3E2723",
@@ -48,14 +41,12 @@ export default function App({ Component, pageProps }) {
           text: {
             primary: mode === "light" ? "#3E2723" : "#FAD9CF",
             secondary: mode === "light" ? "#5D4037" : "#D7CCC8",
-            accent: "#2F4F4F",
           },
         },
         typography: {
           fontFamily: "'Bodoni Moda', serif",
           h1: { fontWeight: 700, fontSize: "2.4rem" },
           h2: { fontWeight: 600, fontSize: "2rem" },
-          h6: { fontWeight: 500 },
           body1: { fontSize: "1rem", lineHeight: 1.6 },
         },
       }),
@@ -65,8 +56,8 @@ export default function App({ Component, pageProps }) {
   return (
     <>
       <Head>
+        <title>Dashboard Administrativo - FAP Mendoza</title>
         <link rel="icon" href="/favicon.ico" />
-        <title>FAP Mendoza</title>
       </Head>
       <ThemeProvider theme={theme}>
         <CssBaseline />
@@ -75,3 +66,5 @@ export default function App({ Component, pageProps }) {
     </>
   );
 }
+
+export default MyApp;
