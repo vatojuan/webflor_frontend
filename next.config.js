d3fed856bab288f1a07f37af5ffac6039aabb5d9
vatojuan <juanchi190_16@hotmail.com>
2025-05-28 04:03:11 -0300

    bdemail234567890-012

--- next.config.js
+++ next.config.js
@@ -1,12 +1,13 @@
 // next.config.js
+/** @type {import('next').NextConfig} */
 const withTM = require('next-transpile-modules')([
-  '@mui/x-data-grid',
+  '@mui/x-data-grid'
 ]);
 
 module.exports = withTM({
   reactStrictMode: true,
   webpack(config) {
-    // Permitimos procesar los CSS que DataGrid importa internamente
+    // Permite cargar el CSS que DataGrid importa desde node_modules
     config.module.rules.push({
       test: /\.css$/,
       include: /node_modules\/@mui\/x-data-grid/,
